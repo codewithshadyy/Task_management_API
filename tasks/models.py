b527dea92da6e6c0029321426c76a6e9d0ba6d5f
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-03-16 15:21:51 +0000

    fixing choices format

--- tasks/models.py
+++ tasks/models.py
@@ -3,17 +3,17 @@ from django.db import models
 from django.conf import settings
 
 class Task(models.Model):
-    PRIORITY_LEVEL = (
+    PRIORITY_LEVEL = [
         ('low', 'low'),
         ('medium', 'medium'),
         ('high', 'high')
-    )
+    ]
     
-    TASK_STATUS = (
+    TASK_STATUS = [
         ("pending", "pending"),
         ("completed", "compleleted"),
         ("cancelled", "cancelled"),
-    )
+    ]
     title = models.CharField()
     description = models.TextField()
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="tasks")

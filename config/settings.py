ab8ed51c75ffa2b19a1a841697f8e489c4e734d5
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-03-17 18:45:28 +0000

    Enabling cors

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'users',
     'tasks',
     'django_filters',
+     'corsheaders',
     
     
 ]
@@ -54,9 +55,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
+CORS_ALLOW_ALL_ORIGINS = True 
+
 
 TEMPLATES = [
     {
